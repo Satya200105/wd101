10c54f994d94c46200b4f9697f73fb7be4b075d9
Satya200105 <151603031+Satya200105@users.noreply.github.com>
2023-11-21 12:33:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-// Function to handle form submission
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevents the default form submission
+// Function to validate the date of birth between 18 and 55 years
+function validateDOB() {
+    const dobInput = document.getElementById("dob");
+    const dob = new Date(dobInput.value);
+    const today = new Date();
+    const minAge = 18;
+    const maxAge = 55;
 
-    // Get form values
+    const diffInMs = today - dob;
+    const ageDate = new Date(diffInMs);
+    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    if (isNaN(dob.getTime())) {
+        alert("Please enter a valid date of birth.");
+        return false;
+    }
+
+    if (age < minAge || age > maxAge) {
+        alert("Age must be between 18 and 55 years.");
+        return false;
+    }
+
+    return true;
+}
+
+// Modify the existing form validation function to include date of birth validation
+function validateForm() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const dob = document.getElementById("dob").value;
-    const acceptTerms = document.getElementById("acceptTerms").checked;
 
-    // Add form data to the table
-    const table = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
-    const newRow = table.insertRow(table.rows.length);
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-    const cell3 = newRow.insertCell(2);
-    const cell4 = newRow.insertCell(3);
-    const cell5 = newRow.insertCell(4);
+    if (name === "" || email === "" || password === "") {
+        alert("Please fill in all fields.");
+        return false;
+    }
 
-    cell1.innerHTML = name;
-    cell2.innerHTML = email;
-    cell3.innerHTML = password;
-    cell4.innerHTML = dob;
-    cell5.innerHTML = acceptTerms ? "Yes" : "No";
+    if (!validateDOB()) {
+        return false;
+    }
 
-    // Clear form fields after submission
-    document.getElementById("registrationForm").reset();
+    // Other validations such as email format, password strength, etc., can be added here
+
+    return true;
+}
+
+// Add event listener to the form for submission
+const registrationForm = document.getElementById("registrationForm");
+registrationForm.addEventListener("submit", function(event) {
+    if (!validateForm()) {
+        event.preventDefault(); // Prevent form submission if validation fails
+    }
 });
