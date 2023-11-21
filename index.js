3b3360f09512b7442abc0b7d770bf8c255ff7df9
Satya200105 <151603031+Satya200105@users.noreply.github.com>
2023-11-21 12:37:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
-// Function to validate the date of birth between 18 and 55 years
-function validateDOB() {
-    const dobInput = document.getElementById("dob");
-    const dob = new Date(dobInput.value);
-    const today = new Date();
-    const minAge = 18;
-    const maxAge = 55;
-
-    const diffInMs = today - dob;
-    const ageDate = new Date(diffInMs);
-    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
-
-    if (isNaN(dob.getTime())) {
-        alert("Please enter a valid date of birth.");
-        return false;
-    }
-
-    if (age < minAge || age > maxAge) {
-        alert("Age must be between 18 and 55 years.");
-        return false;
-    }
-
-    return true;
-}
-
-// Modify the existing form validation function to include date of birth validation
-function validateForm() {
+// Function to add form data to the table and localStorage
+function addDataToTable() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const acceptTerms = document.getElementById("acceptTerms").checked;
 
-    if (name === "" || email === "" || password === "") {
-        alert("Please fill in all fields.");
-        return false;
-    }
+    // Add data to table
+    const table = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
+    const newRow = table.insertRow(table.rows.length);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
 
-    if (!validateDOB()) {
-        return false;
-    }
+    cell1.innerHTML = name;
+    cell2.innerHTML = email;
+    cell3.innerHTML = password;
+    cell4.innerHTML = dob;
+    cell5.innerHTML = acceptTerms ? "Yes" : "No";
 
-    // Other validations such as email format, password strength, etc., can be added here
+    // Save data to localStorage
+    const newData = {
+        name: name,
+        email: email,
+        password: password,
+        dob: dob,
+        acceptTerms: acceptTerms
+    };
 
-    return true;
+    let existingData = JSON.parse(localStorage.getItem("formData")) || [];
+    existingData.push(newData);
+    localStorage.setItem("formData", JSON.stringify(existingData));
 }
 
-// Add event listener to the form for submission
-const registrationForm = document.getElementById("registrationForm");
-registrationForm.addEventListener("submit", function(event) {
-    if (!validateForm()) {
-        event.preventDefault(); // Prevent form submission if validation fails
-    }
+// Function to load data from localStorage
+function loadTableData() {
+    const table = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
+    let existingData = JSON.parse(localStorage.getItem("formData")) || [];
+
+    existingData.forEach(data => {
+        const newRow = table.insertRow(table.rows.length);
+        const cell1 = newRow.insertCell(0);
+        const cell2 = newRow.insertCell(1);
+        const cell3 = newRow.insertCell(2);
+        const cell4 = newRow.insertCell(3);
+        const cell5 = newRow.insertCell(4);
+
+        cell1.innerHTML = data.name;
+        cell2.innerHTML = data.email;
+        cell3.innerHTML = data.password;
+        cell4.innerHTML = data.dob;
+        cell5.innerHTML = data.acceptTerms ? "Yes" : "No";
+    });
+}
+
+// Call loadTableData() to load existing data on page load
+window.onload = loadTableData;
+
+// Modify the form submission function to include adding data to the table and localStorage
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevents the default form submission
+    addDataToTable();
+    this.reset(); // Clear form fields after submission
 });
