d10496967de4a5500c78a66abf6e64ddf5dcd3f4
Satya200105 <151603031+Satya200105@users.noreply.github.com>
2023-11-21 14:03:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,30 @@
-function validateDOB() {
-    const dobInput = document.getElementById("dob");
-    const dob = new Date(dobInput.value);
-    const today = new Date();
-    const minAge = 18;
-    const maxAge = 55;
+const registrationForm = document.getElementById('registrationForm');
+const userDataContainer = document.getElementById('userDataContainer');
+const userDataTable = userDataContainer.querySelector('table tbody');
 
-    const diffInMs = today - dob;
-    const ageDate = new Date(diffInMs);
-    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+const savedUserData = JSON.parse(localStorage.getItem('userData') || '[]');
+populateUserDataTable(savedUserData);
 
-    if (isNaN(dob.getTime())) {
-        alert("Please enter a valid date of birth.");
-        return false;
-    }
+registrationForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-    if (age < minAge || age > maxAge) {
-        alert("Age must be between 18 and 55 years.");
-        return false;
-    }
+  const formData = new FormData(registrationForm);
+  const userData = {
+    name: formData.get('name'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+    dob: formData.get('dob'),
+    terms: formData.get('terms') === 'on',
+  };
 
-    return true;
-}// Function to add form data to the table and localStorage
-function addDataToTable() {
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const dob = document.getElementById("dob").value;
-    const acceptTerms = document.getElementById("acceptTerms").checked;
+  // Validate date of birth
+  const dobDate = new Date(userData.dob);
+  const today = new Date();
+  const age = today.getFullYear() - dobDate.getFullYear();
+  if (age < 18 || age > 55) {
+    alert('Date of birth must be between 18 and 55 years old.');
+    return;
+  }
 
-    // Add data to table
-    const table = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
-    const newRow = table.insertRow(table.rows.length);
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-    const cell3 = newRow.insertCell(2);
-    const cell4 = newRow.insertCell(3);
-    const cell5 = newRow.insertCell(4);
-
-    cell1.innerHTML = name;
-    cell2.innerHTML = email;
-    cell3.innerHTML = password;
-    cell4.innerHTML = dob;
-    cell5.innerHTML = acceptTerms ? "Yes" : "No";
-
-    // Save data to localStorage
-    const newData = {
-        name: name,
-        email: email,
-        password: password,
-        dob: dob,
-        acceptTerms: acceptTerms
-    };
-
-    let existingData = JSON.parse(localStorage.getItem("formData")) || [];
-    existingData.push(newData);
-    localStorage.setItem("formData", JSON.stringify(existingData));
-}
-// Function to load data from localStorage
-function loadTableData() {
-    const table = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
-    let existingData = JSON.parse(localStorage.getItem("formData")) || [];
-
-    existingData.forEach(data => {
-        const newRow = table.insertRow(table.rows.length);
-        const cell1 = newRow.insertCell(0);
-        const cell2 = newRow.insertCell(1);
-        const cell3 = newRow.insertCell(2);
-        const cell4 = newRow.insertCell(3);
-        const cell5 = newRow.insertCell(4);
-
-        cell1.innerHTML = data.name;
-        cell2.innerHTML = data.email;
-        cell3.innerHTML = data.password;
-        cell4.innerHTML = data.dob;
-        cell5.innerHTML = data.acceptTerms ? "Yes" : "No";
-    });
-}
-
-// Call loadTableData() to load existing data on page load
-window.onload = loadTableData;
-
-// Modify the form submission function to include adding data to the table and localStorage
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevents the default form submission
-    addDataToTable();
-    this.reset(); // Clear form fields after submission
-});
+  savedUserData.push(userData);
+  localStorage.setItem('userData', JSON.stringify(savedUserData));
